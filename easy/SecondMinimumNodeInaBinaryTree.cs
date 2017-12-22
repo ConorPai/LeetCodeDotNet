4b4c825ba5b461e22a83acd1b2c50e26c2cccb34
ConorPai <baiguangnan@163.com>
2017-12-22 13:44:53 +0800

    CommitCode

--- easy/SecondMinimumNodeInaBinaryTree.cs
+++ easy/SecondMinimumNodeInaBinaryTree.cs
@@ -4,30 +4,24 @@ using System;
 
 public class SecondMinimumNodeInaBinaryTreeDemo {
     public int FindSecondMinimumValue(TreeNode root) {
-        if (root == null) {
-        return -1;
-        }
-        if (root.left == null && root.right == null) {
+        if (root == null)
             return -1;
-        }
+        if (root.left == null && root.right == null)
+            return -1;
         
         int left = root.left.val;
         int right = root.right.val;
         
-        // if value same as root value, need to find the next candidate
-        if (root.left.val == root.val) {
+        if (root.left.val == root.val)
             left = FindSecondMinimumValue(root.left);
-        }
-        if (root.right.val == root.val) {
+        if (root.right.val == root.val)
             right = FindSecondMinimumValue(root.right);
-        }
         
-        if (left != -1 && right != -1) {
+        if (left != -1 && right != -1)
             return Math.Min(left, right);
-        } else if (left != -1) {
+        else if (left != -1)
             return left;
-        } else {
+        else
             return right;
-        }
     }
 }
