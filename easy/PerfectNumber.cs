6c0573d12d4b05b5ed5c248fbe10b98c64740c80
ConorPai <baiguangnan@163.com>
2018-01-23 18:01:35 +0800

    CommitCode

---
+++ easy/PerfectNumber.cs
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/perfect-number/description/
+
+using System;
+
+public class PerfectNumberDemo {
+    public bool CheckPerfectNumber(int num) {
+        if (num == 1) return false;
+        
+        int sum = 0;
+        for (int i = 2; i <= Math.Sqrt(num); i++) {
+            if (num % i == 0) {
+                sum += i + num / i;
+            }
+        }
+        sum++;
+        
+        return sum == num;
+    }
+}
