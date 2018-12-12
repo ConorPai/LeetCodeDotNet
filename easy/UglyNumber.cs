12c6114223a5a1a2c9776566e02b1da8b433d59e
ConorPai <baiguangnan@163.com>
2018-12-12 16:23:52 +0800

    CommitCode

---
+++ easy/UglyNumber.cs
@@ -0,0 +1,18 @@
+//https://leetcode-cn.com/problems/ugly-number/comments/
+
+public class UglyNumberDemo {
+    public bool IsUgly(int num) {
+        
+        if (num <= 0)
+            return false;
+
+        while (num % 5 == 0)
+            num /= 5;
+        while (num % 3 == 0)
+            num /= 3;
+        while (num % 2 == 0)
+            num /= 2;
+
+        return num == 1;
+    }
+}
