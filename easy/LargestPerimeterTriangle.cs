d24819c88fff1f60f4272674f9dd2a7130e20483
ConorPai <baiguangnan@163.com>
2019-01-17 15:18:21 +0800

    CommitCode

---
+++ easy/LargestPerimeterTriangle.cs
@@ -0,0 +1,17 @@
+//https://leetcode-cn.com/problems/largest-perimeter-triangle/comments/
+
+using System;
+
+public class LargestPerimeterTriangleDemo {
+    public int LargestPerimeter(int[] A) {
+        Array.Sort(A);
+
+        for (int i = A.Length - 3; i >= 0; i--)
+        {
+            if (A[i] + A[i + 1] > A[i + 2])
+                return A[i] + A[i + 1] + A[i + 2];
+        }
+
+        return 0;
+    }
+}
