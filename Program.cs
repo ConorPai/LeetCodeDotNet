5f8b076e4512ebb6831a8fada1ac08d31797b0c6
ConorPai <baiguangnan@163.com>
2019-02-18 18:20:13 +0800

    CommitCode

--- Program.cs
+++ Program.cs
@@ -874,6 +874,9 @@ namespace dotnetcoreproject
 
             SimplifyPathDemo test207 = new SimplifyPathDemo();
             Console.WriteLine(test207.SimplifyPath("/a//b////c/d//././/.."));
+
+            AddtoArrayFormofIntegerDemo test208 = new AddtoArrayFormofIntegerDemo();
+            IList<int> ret208 = test208.AddToArrayForm(new int[]{1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,0,6,3}, 516);
         }
     }
 }
---
+++ easy/AddtoArrayFormofInteger.cs
@@ -0,0 +1,21 @@
+//https://leetcode-cn.com/problems/add-to-array-form-of-integer/comments/
+
+using System;
+using System.Collections.Generic;
+
+public class AddtoArrayFormofIntegerDemo {
+    public IList<int> AddToArrayForm(int[] A, int K) {
+        
+        var m = 0;
+        var r = new List<int>();
+        
+        for(var i = A.Length - 1; i >= 0 || K > 0 || m > 0; i--){
+            var c = m + (i >= 0 ? A[i] : 0) + K % 10;
+            m = c / 10;
+            K /= 10;
+            r.Insert(0, c % 10);
+        }
+        
+        return r;
+    }
+}
