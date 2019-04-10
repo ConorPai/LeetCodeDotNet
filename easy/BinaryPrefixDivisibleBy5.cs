e6f31375d2309ba61e599a2a4c553fe0194e6150
ConorPai <baiguangnan@163.com>
2019-04-10 16:25:47 +0800

    CommitCode

---
+++ easy/BinaryPrefixDivisibleBy5.cs
@@ -0,0 +1,19 @@
+//https://leetcode-cn.com/problems/binary-prefix-divisible-by-5/comments/
+
+using System.Collections.Generic;
+
+public class BinaryPrefixDivisibleBy5Demo {
+    public IList<bool> PrefixesDivBy5(int[] A) {
+        IList<bool> bRet = new List<bool>();
+
+        int value = 0;
+        foreach (int a in A)
+        {
+            value = (value << 1) + a;
+            bRet.Add(value % 5 == 0);
+            value %= 5;
+        }
+
+        return bRet;
+    }
+}
