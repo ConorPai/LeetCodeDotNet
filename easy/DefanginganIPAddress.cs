f51f9a165d0bdefec8754375bf8fba2c4af62173
ConorPai <baiguangnan@163.com>
2019-07-11 18:51:02 +0800

    CommitCode

---
+++ easy/DefanginganIPAddress.cs
@@ -0,0 +1,7 @@
+//https://leetcode-cn.com/problems/defanging-an-ip-address/
+
+public class DefanginganIPAddressDemo {
+    public string DefangIPaddr(string address) {
+        return address.Replace(".", "[.]");
+    }
+}
