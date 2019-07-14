1d33487776b5734e8a5d1f592a0c78508f2c2484
ConorPai <baiguangnan@163.com>
2019-07-14 15:43:54 +0800

    CommitCode

---
+++ easy/PartitionArrayIntoThreePartsWithEqualSum.cs
@@ -0,0 +1,27 @@
+//https://leetcode-cn.com/problems/partition-array-into-three-parts-with-equal-sum/
+
+public class Solution {
+    public bool CanThreePartsEqualSum(int[] A) {
+        int nSum = 0;
+        foreach(int a in A)
+            nSum += a;
+
+        if (nSum % 3 != 0)
+            return false;
+
+        int nAvg = nSum / 3;
+        int nTimes = 0;
+        int nTotal = 0;
+        foreach (int a in A)
+        {
+            nTotal += a;
+            if (nTotal == nAvg)
+            {
+                nTimes++;
+                nTotal = 0;
+            }
+        }
+
+        return nTimes == 3;
+    }
+}
