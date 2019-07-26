84fc6db82d44766046a842d0351961da58be4529
ConorPai <baiguangnan@163.com>
2019-07-26 17:15:34 +0800

    CommitCode

---
+++ medium/GenerateParentheses.cs
@@ -0,0 +1,31 @@
+//https://leetcode-cn.com/problems/generate-parentheses/
+
+using System.Collections.Generic;
+
+public class Solution {
+    public IList<string> GenerateParenthesis(int n) {
+        if (n == 0)
+        {
+            return new List<string>();
+        }
+
+        if (n == 1)
+        {
+            return new List<string>() { { "()" } };
+        }
+
+        var nMinus1 = GenerateParenthesis(n - 1);
+
+        var result = new Dictionary<string, bool>();
+
+        foreach (var r in nMinus1)
+        {
+            for (int i = 0; i < r.Length; i++)
+            {
+                result[r.Insert(i, "()")] = true;
+            }
+        }
+
+        return new List<string>(result.Keys);
+    }
+}
