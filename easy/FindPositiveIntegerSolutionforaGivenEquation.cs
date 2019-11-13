83d3348ffc123d310711aac089834f125c3198ff
ConorPai <baiguangnan@163.com>
2019-11-13 17:31:23 +0800

    CommitCode

---
+++ easy/FindPositiveIntegerSolutionforaGivenEquation.cs
@@ -0,0 +1,31 @@
+//https://leetcode-cn.com/problems/find-positive-integer-solution-for-a-given-equation/
+
+public class CustomFunction {
+    public int f(int x, int y)
+    {
+        return x + y;
+    }
+};
+
+public class FindPositiveIntegerSolutionforaGivenEquationDemo {
+    public IList<IList<int>> FindSolution(CustomFunction customfunction, int z) {
+        IList<IList<int>> res = new List<IList<int>>();
+        int left = 1;
+        int right = 1000;
+        while (left <= 1000 && right >= 1) {
+            int temp = customfunction.f(left,right);
+            if (temp == z) {
+                IList<int> list = new List<int>();
+                list.Add(left);
+                list.Add(right);
+                res.Add(list);
+                left++;
+            } else if (temp > z) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return res;
+    }
+}
