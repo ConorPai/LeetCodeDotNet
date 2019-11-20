bfbe50bd5cb0758757cf7f4b6eb26972be57367d
ConorPai <baiguangnan@163.com>
2019-11-20 17:54:29 +0800

    CommitCode

---
+++ easy/Shift2DGrid.cs
@@ -0,0 +1,21 @@
+//https://leetcode-cn.com/problems/shift-2d-grid/
+
+public class Shift2DGridDemo {
+    public IList<IList<int>> ShiftGrid(int[][] grid, int k) {
+        int n = grid.Length;
+        int m = grid[0].Length;
+        int ttl = n * m;
+
+        IList<IList<int>> res = new List<IList<int>>();
+
+        for(int i = (ttl - k % ttl) % ttl, j = 0; j < ttl; j++, i=(++i) % ttl)
+        {
+            if(j % m == 0)
+                res.Add(new List<int>());
+
+            res[res.Count - 1].Add(grid[i / m][i % m]);
+        }
+
+        return res;
+    }
+}
