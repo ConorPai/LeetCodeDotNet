214db5e8bb9f95ed994e29a77ae9db53890c7fd6
ConorPai <baiguangnan@163.com>
2019-11-28 16:52:33 +0800

    CommitCode

---
+++ easy/RangeAdditionII.cs
@@ -0,0 +1,21 @@
+//https://leetcode-cn.com/problems/range-addition-ii/
+
+class RangeAdditionIIDemo {
+    public int maxCount(int m, int n, int[][] ops) {
+        if (ops == null || ops.Length == 0)
+            return m * n;
+
+        int nX = Int32.MaxValue;
+        int nY = Int32.MaxValue;
+
+        for (int i = 0; i < ops.Length; i++)
+        {
+            if (ops[i][0] < nX)
+                nX = ops[i][0];
+            if (ops[i][1] < nY)
+                nY = ops[i][1];
+        }
+
+        return nX * nY;
+    }
+}
